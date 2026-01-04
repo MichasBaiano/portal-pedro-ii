24bdbadccafc556d1e04d017191be41a2c96c20b
MichasBaiano <programamichel@gmail.com>
2026-01-04 01:42:55 -0300

    ✨ feat(index): adiciona banners de propragando no home

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,20 +1,23 @@
 import { EventosModel } from "../model/eventosModel.js";
 import { EstabelecimentosModel } from "../model/estabelecimentoModel.js";
+import { BannersModel } from "../model/bannersModel.js";
 
 export class HomeController {
     static async index(req, res) {
         try {
             const eventos = await EventosModel.getProximos(3);
             const destaques = await EstabelecimentosModel.getDestaques(3);
+            const banners = await BannersModel.getAll();
 
             res.render("index", { 
                 eventos, 
-                destaques 
+                destaques,
+                banners
             });
 
         } catch (erro) {
             console.error("Erro na Home:", erro);
-            res.render("index", { eventos: [], destaques: [] });
+            res.render("index", { eventos: [], destaques: [], banners: [] });
         }
     }
 }
